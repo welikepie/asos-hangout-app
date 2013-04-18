02c9dbe2a343fb6033a46f96428654e8af19a92d
Mikołaj Banasik <d1sover@gmail.com>
2013-04-18 20:11:27 +0100

    HTML templates for the app pages.

--- gruntfile.js
+++ gruntfile.js
@@ -14,10 +14,11 @@ module.exports = function (grunt) {
 					'compile': false,
 					'compress': false,
 					'noIDs': false,
-					'noOverqualifying': false
+					'noOverqualifying': false,
+					'noUniversalSelectors': false,
+					'strictPropertyOrder': false
 				}
 			},
-
 			'landing-page-dev': {
 				'options': {
 					'compile': true,
@@ -26,7 +27,6 @@ module.exports = function (grunt) {
 				'src': 'src/landing-page/styles/*.less',
 				'dest': 'build/landing-page/styles/styles.css'
 			},
-
 			'landing-page-release': {
 				'options': {
 					'compile': true,
@@ -34,12 +34,42 @@ module.exports = function (grunt) {
 				},
 				'src': 'src/landing-page/styles/*.less',
 				'dest': 'build/landing-page/styles/styles.css'
+			},
+
+			'hangout-app-lint': {
+				'src': 'src/hangout-app/styles/*.less',
+				'options': {
+					'compile': false,
+					'compress': false,
+					'noIDs': false,
+					'noOverqualifying': false,
+					'noUniversalSelectors': false,
+					'strictPropertyOrder': false
+				}
+			},
+			'hangout-app-dev': {
+				'options': {
+					'compile': true,
+					'compress': false
+				},
+				'src': 'src/hangout-app/styles/*.less',
+				'dest': 'build/hangout-app/styles/styles.css'
+			},
+			'hangout-app-release': {
+				'options': {
+					'compile': true,
+					'compress': true
+				},
+				'src': 'src/hangout-app/styles/*.less',
+				'dest': 'build/hangout-app/styles/styles.css'
 			}
+
 		},
 
 		'clean': {
 			'build': { 'src': ['build'] },
-			'landing-page': { 'src': ['build/landing-page'] }
+			'landing-page': { 'src': ['build/landing-page'] },
+			'hangout-app': { 'src': ['build/hangout-app'] }
 		},
 
 		'copy': {
@@ -51,6 +81,32 @@ module.exports = function (grunt) {
 				'src': '**/*.!(less)',
 				'cwd': 'src/landing-page/styles/',
 				'dest': 'build/landing-page/styles/',
+				'expand': true,
+				'filter': 'isFile'
+			},
+			'landing-page-images': {
+				'src': ['**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.png'],
+				'cwd': 'src/landing-page/images/',
+				'dest': 'build/landing-page/images/',
+				'expand': true,
+				'filter': 'isFile'
+			},
+
+			'hangout-app-html': {
+				'src': 'src/hangout-app/index.html',
+				'dest': 'build/hangout-app/index.html'
+			},
+			'hangout-app-styling': {
+				'src': '**/*.!(less)',
+				'cwd': 'src/hangout-app/styles/',
+				'dest': 'build/hangout-app/styles/',
+				'expand': true,
+				'filter': 'isFile'
+			},
+			'hangout-app-images': {
+				'src': ['**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.png'],
+				'cwd': 'src/hangout-app/images/',
+				'dest': 'build/hangout-app/images/',
 				'expand': true,
 				'filter': 'isFile'
 			}
@@ -71,8 +127,17 @@ module.exports = function (grunt) {
 		'recess:landing-page-lint',
 		'recess:landing-page-dev',
 		'copy:landing-page-styling',
+		'copy:landing-page-images',
 		'copy:landing-page-html'
 	]);
-	grunt.registerTask('landing-page', 'landing-page-dev');
+
+	grunt.registerTask('hangout-app-dev', [
+		'clean:hangout-app',
+		'recess:hangout-app-lint',
+		'recess:hangout-app-dev',
+		'copy:hangout-app-styling',
+		'copy:hangout-app-images',
+		'copy:hangout-app-html'
+	]);
 
 };
