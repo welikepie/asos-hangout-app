0190e6473952426f73c64b215dd4218d4a9fce91
Mikołaj Banasik <d1sover@gmail.com>
2013-06-11 20:12:25 +0100

    Moved interface code to separate file, started up front-end.

--- src/admin/fashion/index.php
+++ src/admin/fashion/index.php
@@ -3,64 +3,84 @@
 	<head>
 		<title>Product Feed Management</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" href="../../common/styles/bootstrap.css" type="text/css">
-		<link rel="stylesheet" href="../../common/styles/flat-ui.css" type="text/css">
-		<link rel="stylesheet" href="styles/custom_addon.css" type="text/css">
-		<script type="text/javascript">
-		window.authToken = <?php
-			if (function_exists("apache_request_headers")) {
-				$headers = apache_request_headers();
-				if (isset($headers['Authorization'])) { $token = $headers['Authorization']; }
-				else { $token = null; }
-			}
-			elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) { $token = $_SERVER['HTTP_AUTHORIZATION']; }
+		<?php
+
+		require_once('../gateway/gateway.php');
+		$handler = new Dummy();
+
+		// Obtain list of all product categories
+		GatewayInterface::getCategories($handler, $handler);
+		$categories = json_decode($handler->buffer[0], true);
+		unset($handler);
+
+		// Insert authorisation token
+		if (function_exists("apache_request_headers")) {
+			$headers = apache_request_headers();
+			if (isset($headers['Authorization'])) { $token = $headers['Authorization']; }
 			else { $token = null; }
-			echo(json_encode($token));
-		?>;
-		</script>
-		<script type="text/javascript" src="../../common/scripts/vendor/require.js" data-main="scripts/main"></script>
+		}
+		elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) { $token = $_SERVER['HTTP_AUTHORIZATION']; }
+		else { $token = null; }
+		echo('<script type="text/javascript">window.authToken = ' . json_encode($token) . ';</script>');
+
+		?>
 	</head>
 	<body>
-		<div id="container" class="well">
-			<div class="column">
-				<div class="columnHeader">
-					<h1>All outfits.</h1>
-					<input type="textarea" id="searchBox">
-					<button class="btn btn-warning">Search</button>
-				</div>
-				<ul id="fullList">
-					<li class="template">
-						<div class="container">
-							<img class="image">
-							<div class="middleColumn">
-								<h4 class="name"></h4>
-								<div class="description"></div>
-								<a class="url">View this item on the site</a>
-							</div>
-							<button class="btn btn-success">Add</button>
-						</div>
-					</li>
-				</ul>
+
+		<form>
+			<h2>Search for products</h2>
+			<fieldset class="categories">
+				<legend>Categories</legend>
+				<label><input type="checkbox" class="all-categories" checked> Select/Deselect All</label>
+				<div><?php foreach ($categories as &$item) {
+					echo('<label><input type="checkbox" name="category" value="' . $item['id'] . '" checked> ' . $item['name'] . '</label>');
+				} ?></div>
+			</fieldset>
+			<fieldset class="misc">
+
+				<label>Name: <input type="text" name="name" value=""></label>
+				<label>Gender: <div>
+					<label><input type="radio" name="gender" value="female"> Woman</label>
+					<label><input type="radio" name="gender" value="male"> Man</label>
+					<label><input type="radio" name="gender" value="" checked> Either</label>
+				</div></label>
+
+			</fieldset>
+			<div class="buttons">
+				<button type="submit">Search</button>
+				<span class="loading"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAABc0lEQVQ4jbWUW4ujQBBG8/9/myAdtJEoIZA0EcWkvESjdHL2YUmPmswos+wHPlh2na6btWGF6rqmrus1R9nMDdZamqbBWutsIoKIuPdhGMjznGEYloF93yMik4jmwCzLMMbQNM0y8Pl8UlUVIkLf92/A2+2GMYY0Tb9P+fF4OOdXSiJCWZbAtIbn8xljDF3XAX9LVBSFS3/zchARqqpyH7quc05jjaOtqoo4jtFaczgcvoDWWgcVkY+gudI0RWuN1pr9fu98JjXs+56yLFcDoyjier1O7G9N+VdtxqnOx2VJSZIQBIF7kiT5D8DfpPWTPjblfr8vOqZpShAEXC6Xd+BvxsYYg1IKpRS73Y62bb+A3w3269BYx+OR0+kEQFmWaK1RSpEkyTTCn369MAwJwxCA7XaL7/uTZZHn+TTCsT4tB8/z8DwPgKIo8H3fXTDXqvU1BgJEUYTv+2RZtgx8NWi8YOfAtm2J4/hjjVfN4biGS/oDazIVUuCNuEcAAAAASUVORK5CYII=" width="20" height="20"> Searching...</span>
 			</div>
-			<div class="column">
-				<div class="columnHeader">
-					<h1> Outfits selected.</h1>
-					<button class="btn btn-warning clear-all">Clear All</button>
-				</div>
-				<ul id="partialList">
-					<li class="template">
-						<div class="container">
-							<img class="image">
-							<div class="middleColumn">
-								<h4 class="name"></h4>
-								<div class="description"></div>
-								<a class="url">View this item on the site</a>
-							</div>
-							<button class="btn btn-danger">Remove</button>
-						</div>
-					</li>
-				</ul>
-			</div>
+		</form>
+
+		<div id="search-results" class="product-list">
+			<h2>Search Results</h2>
+			<ul>
+
+				<li class="template">
+					<img src="" class="image">
+					<h3 class="name"></h3>
+					<div class="description"></div>
+					<a href="" target="_blank" class="url">View on site</a>
+					<button type="button">Add to product feed</button>
+				</li>
+
+			</ul>
 		</div>
+
+		<div id="product-feed" class="product-list">
+			<h2>Product Feed</h2>
+			<ul>
+
+				<li class="template">
+					<img src="" class="image">
+					<h3 class="name"></h3>
+					<div class="description"></div>
+					<a href="" target="_blank" class="url">View on site</a>
+					<button type="button">Remove from product feed</button>
+				</li>
+
+			</ul>
+		</div>
+
 	</body>
 </html>
---
+++ src/admin/gateway/gateway.php
@@ -0,0 +1,250 @@
+<?php
+
+	if (!defined('JSON_PRETTY_PRINT')) { define('JSON_PRETTY_PRINT', 0); }
+	set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+	    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+	});
+
+	class GatewayInterface {
+
+		protected static $db = null;
+		protected static function open_db () {
+			try {
+				static::$db = new mysqli(
+					'welikepie.com',
+					'alex',
+					'Sannevanwel1j',
+					'asos',
+					3306
+				);
+				if (self::$db->connect_error) {
+					throw new Exception(sprintf('[%d] %s', self::$db->connect_errno, self::$db->connect_error));
+				}
+			} catch (Exception $e) {
+				self::$db = null;
+				throw $e;
+			}
+		}
+		protected static function close_db () {
+			if (isset(self::$db)) {
+				// Kill the thread, thus closing the TCP connection
+				self::$db->kill(self::$db->thread_id);
+				self::$db->close();
+				self::$db = null;
+			}
+		}
+		protected static function execute_query ($query, $args, $callback = NULL) {
+
+			if (!self::$db) { throw new Exception('Database connection not opened.'); }
+
+			$stmt = self::$db->prepare($query);
+			if (!$stmt) { throw new Exception(sprintf('[%d] %s', self::$db->errno, self::$db->error)); }
+			$sig = array(); $arguments = array();
+			foreach ($args as &$val) {
+				if (is_bool($val)) {
+					$sig[] = 'i';
+					$arguments[] = $val ? 1 : 0;
+				} else if (is_int($val)) {
+					$sig[] = 'i';
+					$arguments[] = $val;
+				} else if (is_float($val)) {
+					$sig[] = 'd';
+					$arguments[] = $val;
+				} else {
+					$sig[] = 's';
+					$arguments[] = strval($val);
+				}
+			}
+			if (count($args)) {
+				$args = array(implode('', $sig));
+				for ($i = 0; $i < count($arguments); $i++) {
+					$bind_name = 'bind' . $i;
+					$$bind_name = $arguments[$i];
+					$args[] = &$$bind_name;
+				}
+				call_user_func_array(array($stmt, 'bind_param'), $args);
+			}
+
+			if (method_exists('mysqli_stmt', 'get_result')) {
+				if ($stmt->execute()) {
+					if ($callback) {
+						$rs = $stmt->get_result();
+						while ($row = $rs->fetch_assoc()) { call_user_func($callback, $row); }
+						$rs->free(); unset($rs);
+					}
+				} else {
+					$error = new Exception(sprintf('[%d] %s', self::$db->errno, self::$db->error));
+				}
+				$stmt->close(); unset($stmt);
+			}
+
+		}
+
+		/* DATA RETRIEVAL
+		 * ******************** */
+		public static function getCategories ($req, $res) {
+
+			// Check for possible match on specific categories
+			if (isset($req->params) && isset($req->params['category_id'])) {
+				$id = $req->params['category_id'];
+				if (is_numeric($id)) { $id = intval($id, 10); }
+				else { $id = null; }
+			} else { $id = null; }
+
+			self::open_db();
+			$result = null;
+			if ($id !== null) {
+				self::execute_query('SELECT id, name FROM categories WHERE id = ?', array($id), function ($row) use (&$result) {
+					$result = array('id' => intval($row['id'], 10), 'name' => $row['name']);
+				});
+			} else {
+				$result = array();
+				self::execute_query('SELECT id, name FROM categories', array(), function ($row) use (&$result) {
+					$result[] = array('id' => intval($row['id'], 10), 'name' => $row['name']);
+				});
+			}
+			self::close_db();
+
+			if ($result !== null) {
+				$res->setFormat('json');
+				$res->add(json_encode($result));
+				$res->send(200);
+			} else {
+				$res->setFormat('json');
+				$res->add(json_encode(array('error' => 'No category with such ID')));
+				$res->send(204);
+			}
+
+		}
+
+		public static $currencies = array('GBP' => '£');
+		public static function getProducts ($req, $res) {
+
+			// Check for possible match on specific categories
+			if (isset($req->params) && isset($req->params['product_id'])) {
+				$id = $req->params['product_id'];
+				if (is_numeric($id)) { $id = intval($id, 10); }
+				else { $id = null; }
+			} else { $id = null; }
+
+			$filters = array(
+				'name' => isset($_GET['name']) ? '%' . $_GET['name'] . '%' : null,
+				'gender' => isset($_GET['gender']) && in_array(strtolower($_GET['gender']), array('male', 'female')) ? strtolower($_GET['gender']) : null,
+				'category' => isset($_GET['category']) ? array_map(function ($val) { return intval($val, 10); }, explode(',', $_GET['category'])) : null,
+				'currency' => isset($_GET['currency']) && array_key_exists(strtoupper($_GET['currency']), self::$currencies) ? strtoupper($_GET['currency']) : 'GBP'
+			);
+
+			// BUILD SQL QUERY
+			$sql_query = array(
+				'SELECT',
+					'products.id,',
+					'products.name,',
+					'products.image,',
+					'products.description,',
+					'categories.id AS category_id,',
+					'categories.name AS category_name,',
+					'product_prices.price',
+				'FROM products',
+				'LEFT OUTER JOIN product_categories ON products.id = product_categories.product_id',
+				'LEFT OUTER JOIN categories ON product_categories.category_id = categories.id',
+				'LEFT OUTER JOIN product_prices ON products.id = product_prices.product_id AND product_prices.currency = ?'
+			);
+			$sql_params = array($filters['currency']);
+			$sql_filters = array();
+
+			// Apply filters
+			if ($id) {
+				$sql_filters[] = 'products.id = ?';
+				$sql_params[] = $id;
+			} else {
+				if ($filters['name']) {
+					$sql_filters[] = 'products.name LIKE ?';
+					$sql_params[] = $filters['name'];
+				}
+				if ($filters['gender']) {
+					$sql_filters[] = 'products.gender = ?';
+					$sql_params[] = $filters['gender'];
+				}
+				if ($filters['category']) {
+					$sql_filters[] = 'product_categories.category_id IN (' . implode(', ', $filters['category']) . ')';
+				}
+			}
+			if (count($sql_filters)) { $sql_query = array_merge($sql_query, array('WHERE'), $sql_filters); }
+
+			$sql_query = array_merge($sql_query, array(
+				'ORDER BY',
+					'products.id ASC,',
+					'product_categories.category_id ASC'
+			));
+
+			/*$res->setFormat('txt');
+			$res->add(implode("\n", $sql_query));
+			$res->send(200);
+			exit();*/
+
+			// Assemble and run the query
+			self::open_db();
+			$result = array();
+			self::execute_query(implode(' ', $sql_query), $sql_params, function ($row) use (&$result, &$filters) {
+
+				$product_id = intval($row['id'], 10);
+				if (!array_key_exists($product_id, $result)) {
+					$result[$product_id] = array(
+						'id' => $product_id,
+						'name' => $row['name'],
+						'photo_small' => $row['image'],
+						'photo_big' => $row['image'],
+						'price' => self::$currencies[$filters['currency']] . number_format(floatval($row['price']), 2),
+						'url' => 'http://www.asos.com/pgeproduct.aspx?iid=' . $product_id,
+						'categories' => array()
+					);
+				}
+				$temp = &$result[$product_id];
+
+				if ($row['description']) { $temp['description'] = $row['description']; }
+				$category_id = strval($row['category_id']);
+				if (!array_key_exists($category_id, $temp['categories'])) {
+					$temp['categories'][$category_id] = $row['category_name'];
+				}
+
+			});
+			self::close_db();
+
+			$result = array_values($result);
+			$res->setFormat('json');
+			$res->add(str_replace('\/', '/', json_encode($id !== null ? $result[0] : $result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)));
+			$res->send(200);
+
+		}
+
+		// ACTUAL FUNCTIONALITY
+		public static function test ($req, $res) {
+
+			$results = array();
+
+			// First, test the database connectivity
+			try {
+				self::open_db();
+				self::close_db();
+				$results['database'] = 'Connection OK';
+			} catch (Exception $e) {
+				$results['database'] = $e->getMessage();
+			}
+
+			$res->setFormat('json');
+			$res->add(json_encode($results, JSON_PRETTY_PRINT));
+			$res->send(200);
+
+		}
+
+	}
+
+	class Dummy {
+		public $params = array();
+		public $buffer = array();
+		public function setFormat () {}
+		public function send () {}
+		public function add ($val) { $this->buffer[] = $val; }
+	}
+
+?>
