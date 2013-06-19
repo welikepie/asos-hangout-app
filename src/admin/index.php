0a700d14ad92045f7cb6bc7797628432b71e7110
Mikołaj Banasik <d1sover@gmail.com>
2013-06-19 18:49:50 +0100

    Redone admin page to keep all options in single page.

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,58 +1,179 @@
-<!DOCTYPE html>
+<?php
+
+	require_once('gateway/gateway.php');
+	$handler = new Dummy();
+
+	// Obtain list of all product categories
+	GatewayInterface::getCategories($handler, $handler);
+	$categories = json_decode($handler->buffer[0], true);
+	unset($handler);
+
+	// Insert authorisation token
+	if (function_exists("apache_request_headers")) {
+		$headers = apache_request_headers();
+		if (isset($headers['Authorization'])) { $token = $headers['Authorization']; }
+		else { $token = null; }
+	}
+	elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) { $token = $_SERVER['HTTP_AUTHORIZATION']; }
+	else { $token = null; }
+
+?><!DOCTYPE html>
 <html>
 	<head>
-		<title>Admin Page For Twitter</title>
+		<title>ASOS Shop-Along Admin</title>
+		<base href="" data-base-url="<%= pkg.app.baseUrl %>" data-node-url="<%= pkg.app.nodeUrl %>">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" href="../common/styles/bootstrap.css" type="text/css">
-		<link rel="stylesheet" href="../common/styles/flat-ui.css" type="text/css">	
-		<link rel="stylesheet" href="styles/custom_addon.css" type="text/css">
-		<script type="text/javascript">
-		window.authToken = <?php
-			if (function_exists("apache_request_headers")) {
-				$headers = apache_request_headers();
-				if (isset($headers['Authorization'])) { $token = $headers['Authorization']; }
-				else { $token = null; }
-			}
-			elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) { $token = $_SERVER['HTTP_AUTHORIZATION']; }
-			else { $token = null; }
-			echo(json_encode($token));
-		?>;
-		</script>
+		<link rel="stylesheet" href="../common/styles/flat-ui.css">
+		<link rel="stylesheet" href="styles/styles.css">
+		<script type="text/javascript">window.authToken = <?php echo(json_encode($token)); ?>;</script>
 		<script type="text/javascript" src="../common/scripts/vendor/require.js" data-main="scripts/main"></script>
 	</head>
 	<body>
-		<div id="container" class="well span7 offset2" style="height:100%;">
+		<div class="menu">
+			<a class="general" href="#general">General Settings</a>
+			<a class="products" href="#products">Product Feed</a>
+			<a class="twitter" href="#twitter">Twitter Feed</a>
+		</div>
+		<div class="root">
 
-			<h1>Administration Page</h1>
-			<h3>ASOS fashion.</h3>
-			<p>Welcome to the back end administration page for the google hangouts.</p>
-			<div id="hangoutEmbed">
-				<input type="text" name="hangoutEmbed">
-				<button type="button">Embed Hangout</button>
-			</div>
-			<div id="categoryLink">
-				<input type="text" name="categoryLink">
-				<button type="button">Change Category Link</button>
-			</div>
-			<div id="liveMessage">
-				<p>Message typed below will be updated live for everyone in the app.</p>
-				<textarea></textarea>
-			</div>
+			<section id="introduction">
+				<header>ASOS Shop-Along Admin</header>
+				<p>This administration panel contains the interface to control the ASOS Shop-Along hangout process, as well as contents of product and Twitter feed.</p>
+			</section>
 
-			<ul>
-				<li>
-					<div class="panelWrapper">
-						<p>The Fashion administration page lets the administrator decide which clothes appear in the hangout app.</p>
-						<a href="fashion" class="btn button">Fashion</a>
+			<section id="general">
+
+				<header>General Settings</header>
+
+				<div class="hangouts">
+					<h2>Hangouts</h2>
+					<p>Link to main hangout:</p>
+					<a href="https://plus.google.com/hangouts/_?hso=0&amp;gid=<%= pkg.app.hangoutAdminAppId %>" target="_blank" class="main">NO HANGOUT - CLICK HERE TO START ONE</a>
+					<p>Link to staging hangout:</p>
+					<a href="https://plus.google.com/hangouts/_?gid=<%= pkg.app.stagingAdminAppId %>" target="_blank" class="staging">NO HANGOUT - CLICK HERE TO START ONE</a>
+					<p>Hangout Live Stream:</p>
+					<a target="_blank" class="embed">NOT AVAILABLE, START ON-AIR HANGOUT FIRST</a>
+				</div>
+					
+				<div class="live-message">
+					<h2>Live Message</h2>
+					<textarea></textarea>
+				</div>
+
+			</section>
+
+			<section id="products">
+
+				<div class="wrapper">
+
+					<header>Product Feed</header>
+
+					<form class="product-search">
+						<fieldset class="categories">
+							<legend>Categories</legend>
+							<label><input type="checkbox" class="all-categories" checked> Select/Deselect All</label>
+							<div><?php foreach ($categories as &$item) {
+								echo('<label><input type="checkbox" name="category" value="' . $item['id'] . '" checked> ' . $item['name'] . '</label>');
+							} ?></div>
+						</fieldset>
+						<fieldset class="misc">
+							<legend>&nbsp;</legend>
+							<label>Name: <input type="text" name="name" value=""></label>
+							<span>Gender: <div>
+								<label><input type="radio" name="gender" value="female"> Woman</label>
+								<label><input type="radio" name="gender" value="male"> Man</label>
+								<label><input type="radio" name="gender" value="" checked> Either</label>
+							</div></span>
+						</fieldset>
+						<div class="buttons">
+							<button type="submit" class="btn btn-success">Search</button>
+							<button type="button" class="btn clear">Clear</button>
+						</div>
+					</form>
+
+					<div id="search-results" class="product-list">
+						<h2>Search Results</h2>
+						<ul>
+
+							<li class="template">
+								<img src="" class="image">
+								<h3 class="name"></h3>
+								<div class="description"></div>
+								<div class="actions">
+									<a href="" target="_blank" class="url">View on site</a>
+									<button type="button" class="btn btn-success">Add to product feed</button>
+								</div>
+							</li>
+
+						</ul>
 					</div>
-				</li>
-				<li>
-					<div class="panelWrapper">
-						<p>The Twitter administration page lets the administrator decide which tweets appear in the hangout app.</p>
-						<a href="twitter" class="btn button">Twitter</a>
+
+					<div id="product-feed" class="product-list">
+						<h2>Product Feed <button type="button" class="btn btn-warning clear-all">Clear All</button></h2>
+						<ul>
+
+							<li class="template">
+								<img src="" class="image">
+								<h3 class="name"></h3>
+								<div class="description"></div>
+								<div class="actions">
+									<a href="" target="_blank" class="url">View on site</a>
+									<button type="button" class="btn btn-danger">Remove from product feed</button>
+								</div>
+							</li>
+
+						</ul>
 					</div>
-				</li>
-			</ul>
+
+				</div>
+
+			</section>
+
+			<section id="twitter">
+
+				<div class="wrapper">
+
+					<header>Twitter Feed</header>
+
+					<div class="filter">
+						<p>Specify the term by which tweets should appear (if empty, no tweets will be streamed).</p>
+						<input type="text" name="twitter_filter" value=""><button type="button" class="btn">Filter</button>
+					</div>
+
+					<div class="product-list incoming-tweets">
+						<h2>Incoming Tweets</h2>
+						<ul>
+
+							<li class="template">
+								<img src="" class="avatar">
+								<h3 class="name"><a href="" target="_blank"></a></h3>
+								<time></time>
+								<div class="content"></div>
+								<button type="button" class="btn btn-success">Add to feed</button>
+							</li>
+
+
+						</ul>
+					</div>
+
+					<div class="product-list twitter-feed">
+						<h2>Twitter Feed</h2>
+						<ul>
+
+							<li class="template">
+								<img src="" class="avatar">
+								<h3 class="name"><a href="" target="_blank"></a></h3>
+								<time></time>
+								<div class="content"></div>
+								<button type="button" class="btn btn-danger">Remove from feed</button>
+							</li>
+
+						</ul>
+					</div>
+
+				</div>
+
+			</section>
 
 		</div>
 	</body>
--- src/admin/scripts/main.js
+++ src/admin/scripts/main.js
@@ -2,74 +2,465 @@ require.config({
 	"baseUrl": "..",
 	"paths": {
 		"jquery": "common/scripts/vendor/jquery-1.9.1.min",
-		"underscore": "common/scripts/vendor/underscore"
+		"backbone": "common/scripts/vendor/backbone",
+		"underscore": "common/scripts/vendor/underscore",
+		"easyXDM": "common/scripts/vendor/easyXDM/easyXDM.min"
 	},
-	"shim": { "underscore": {"exports": "_"} },
+	"shim": {
+		"underscore": {"exports": "_"},
+		"backbone": {
+			"deps": ["underscore", "jquery"],
+			"exports": "Backbone"
+		},
+		"easyXDM": {"exports": "easyXDM"}
+	},
 	"waitSeconds": 10
 });
-require(['jquery', 'underscore'], function ($, _) {
+require([
+	'jquery', 'underscore', 'backbone', 'easyXDM',
+	'common/scripts/ui/collection-view', 'common/scripts/data/product', 'common/scripts/data/tweet'
+], function (
+	$, _, Backbone, easyXDM,
+	CollectionView, Products, Tweets
+) {
 	"use strict";
 
-	var nodeUrlBase = window.location.protocol + '//' + window.location.hostname + ':8888';
+	var loadedProducts = new Products.ProductCollection(),
+		productFeed = new Products.ProductCollection(),
+		productSearchCache = {},
+
+		incomingTweets = new Tweets.TweetCollection(),
+		approvedTweets = new Tweets.TweetCollection(),
+		incomingTweetCount = 50,
+		approvedTweetCount = 20;
+
+	productFeed.comparator = function (a, b) { return (b.get('addedAt') || (new Date()).getTime()) - (a.get('addedAt') || (new Date()).getTime()); };
 
 	$(function () {
 
-		var embedField = $('#hangoutEmbed input'),
-			embedButton = $('#hangoutEmbed  button'),
-			categoryField = $('#categoryLink input'),
-			categoryButton = $('#categoryLink button'),
+		var baseUrl = $('base').eq(0).attr('data-base-url'),
+			nodeUrl = $('base').eq(0).attr('data-node-url'),
 
-			liveMessage = $('#liveMessage textarea');
+			defaultMainLink = $('#general .hangouts .main').attr('href'),
+			defaultStagingLink = $('#general .hangouts .staging').attr('href'),
 
-		$.ajax({
-			'url': nodeUrlBase + '/app-options',
-			'type': 'GET',
-			'dataType': 'json',
-			'success': function (data) {
+			// Backbone Views for controlling product feed and search dialogue
+			productSearchView,
+			productFeedView,
 
-				try { embedField.val('http://youtu.be/' + data.hangoutEmbed.match(/^https?:\/\/(?:www\.)?youtube\.com\/embed\/(.+)$/i)[1]); } catch (e) {}
-				try { categoryField.val(data.categoryLink); } catch (e) {}
-				try { liveMessage.val(data.liveMessage); } catch (e) {}
+			// Backbone Views for controlling Twitter feed
+			incomingTweetsView,
+			approvedTweetsView;
 
-				embedButton.on('click', function () {
-					var match = embedField.val().match(/^https?:\/\/youtu\.be\/(.+)$/i);
-					if (match) { match = 'http://youtube.com/embed/' + match[1]; } else { match = ''; }
+		$('#general .live-message textarea').on('keypress', _.debounce(function () {
+			$.ajax({
+				'url': nodeUrl + 'app-options',
+				'type': 'POST',
+				'dataType': 'text',
+				'cache': false,
+				'data': {'liveMessage': this.value},
+				'headers': { 'Authorization': window.authToken }
+			});
+		}, 1000));
+
+		/* PRODUCT FEED
+		 ********************************** */
+
+		productSearchView = new CollectionView({
+
+			'collection': loadedProducts,
+			'el': $('#products #search-results ul').get(0),
+			'template': $('#products #search-results ul > li.template').remove().removeClass('template'),
+
+			'filter': (function () {
+
+				var filter_func = function (model) {
+					return _.contains(filter_func.collection, model.id);
+				}; filter_func.collection = [];
+
+				return filter_func;
+
+			}()),
+
+			'populate': function (model, element) {
+				var excerpt = document.createElement('div');
+				excerpt.innerHTML = model.get('description');
+				excerpt = $(excerpt).text().substring(0, 140) + "...";
+				$(element)
+					.find('.name').html(model.get('name')).end()
+					.find('.description').html(excerpt).end()
+					.find('.url').attr('href', model.get('url')).end()
+					.find('.image').attr('src', model.get('photo_small')).end();
+			},
+
+			'itemEvents': {
+				'click button': function (model) {
+					if (!productFeed.get(model.get('id'))) {
+
+						$.ajax({
+							'url': nodeUrl + 'product-feed',
+							'type': 'POST',
+							'dataType': 'text',
+							'cache': false,
+							'data': JSON.stringify(model.toJSON()),
+							'headers': { 'Authorization': window.authToken }
+						});
+
+					}
+				}
+			}
+
+		});
+
+		productFeedView = new CollectionView({
+
+			'collection': productFeed,
+			'el': $('#products #product-feed ul').get(0),
+			'template': $('#products #product-feed ul > li.template').remove().removeClass('template'),
+
+			'populate': function (model, element) {
+				var excerpt = document.createElement('div');
+				excerpt.innerHTML = model.get('description');
+				excerpt = $(excerpt).text().substring(0, 140) + "...";
+				$(element)
+					.find('.name').html(model.get('name')).end()
+					.find('.description').html(excerpt).end()
+					.find('.url').attr('href', model.get('url')).end()
+					.find('.image').attr('src', model.get('photo_small')).end();
+			},
+
+			'itemEvents': {
+				'click button': function (model) {
+
 					$.ajax({
-						'url': nodeUrlBase + '/app-options',
-						'type': 'POST',
+						'url': nodeUrl + 'product-feed/' + model.id,
+						'type': 'DELETE',
 						'dataType': 'text',
 						'cache': false,
-						'data': {'hangoutEmbed': match},
 						'headers': { 'Authorization': window.authToken }
 					});
-				});
 
-				categoryButton.on('click', function () {
+				}
+			}
+
+		});
+		productFeedView.listenTo(productFeed, 'add remove sort change sync reset', _.debounce(productFeedView.render, 250));
+
+		productSearchView.render();
+		productFeedView.render();
+
+		// PRODUCT SEARCH FORM HANDLING
+		$('#products form').on('submit', function (ev) {
+
+			var temp,
+				$this = $(this),
+				activator = $this.find('button[type="submit"]'),
+				parameters = {};
+
+			ev.preventDefault();
+			ev.stopPropagation();
+
+			if (!activator.attr('disabled')) {
+
+				// Assemble the search parameters
+				var allCategories = $this.find('input[name="category"]'),
+					enabledCategories = allCategories.filter(':checked');
+
+				if (enabledCategories.length <= 0) {
+					window.alert('You need to keep at least one category selected.');
+					return false;
+				}
+
+				if (allCategories.length !== enabledCategories.length) {
+					parameters['category'] = [];
+					enabledCategories.each(function () { parameters['category'].push(this.value); });
+					parameters['category'] = parameters['category'].join(',');
+				}
+
+				temp = $this.find('input[name="name"]').val().replace(/^\s+|\s+$/g, '');
+				if (temp.length) { parameters['name'] = temp; }
+
+				temp = $this.find('input[name="gender"]').filter(':checked').val();
+				if (temp && temp.length) { parameters['gender'] = temp; }
+
+				// Check if search results are already in hash
+				var hash = JSON.stringify(parameters);
+				if (_.has(productSearchCache, hash)) {
+
+					productSearchView.filter.collection = productSearchCache[hash];
+					productSearchView.render();
+
+				} else {
+
+					activator
+						.attr('disabled', 'disabled')
+						.removeClass('btn-success');
+
 					$.ajax({
-						'url': nodeUrlBase + '/app-options',
-						'type': 'POST',
-						'dataType': 'text',
-						'cache': false,
-						'data': {'categoryLink': categoryField.val()},
-						'headers': { 'Authorization': window.authToken }
-					});
-				});
 
-				liveMessage.on('keypress', _.debounce(function () {
-					var props = {
-						'url': nodeUrlBase + '/app-options',
-						'type': 'POST',
-						'dataType': 'text',
-						'cache': false,
-						'data': {'liveMessage': liveMessage.val()},
-						'headers': { 'Authorization': window.authToken }
-					};
-					console.log('Will send: ', props);
-					$.ajax(props);
-				}, 2000));
+						'url': 'gateway/products',
+						'type': 'GET',
+						'dataType': 'json',
+						'data': parameters,
+
+						'success': function (data) {
+
+							// Add search signature to cache
+							var collection = _.pluck(data, 'id');
+							collection.sort();
+							productSearchCache[hash] = collection;
+							productSearchView.filter.collection = collection;
+
+							loadedProducts.set(data, {'add': true, 'remove': false, 'merge': false});
+							productSearchView.render();
+
+						},
+						'error': function () {
+							window.alert('There was an issue with retrieving the products. Please try again in a moment.');
+						},
+						'complete': function () {
+							activator
+								.removeAttr('disabled')
+								.addClass('btn-success');
+						}
+
+					});
+
+				}
 
 			}
-		})
+
+			return false;
+
+		});
+		$('#products button.clear').on('click', function () {
+			productSearchView.filter.collection = [];
+			productSearchView.render();
+		});
+		// Bind clearing product feed
+		$('#products button.clear-all').on('click', function () {
+			$.ajax({
+				'url': nodeUrl + 'product-feed',
+				'type': 'DELETE',
+				'dataType': 'text',
+				'cache': false,
+				'headers': { 'Authorization': window.authToken }
+			});
+		});
+
+		// Category listing bindings
+		var selectAll = $('#products form input.all-categories').get(0),
+			allCategories = $('#products form input[name="category"]');
+
+		allCategories.on('change', function () {
+			var checkCategories = allCategories.filter(':checked');
+			selectAll.checked = allCategories.length === checkCategories.length;
+		});
+		$(selectAll).on('change', function () {
+			allCategories.each(
+				this.checked ?
+				function () { this.checked = true; } :
+				function () { this.checked = false; }
+			);
+		});
+
+		/* TWITTER FEED
+		 ********************************** */
+
+		 incomingTweetsView = new CollectionView({
+
+			'collection': incomingTweets,
+			'el': $('#twitter .incoming-tweets ul').get(0),
+			'template': $('#twitter .incoming-tweets ul > li.template').remove().removeClass('template'),
+
+			'advancedFilter': function (collection) { return collection.first(incomingTweetCount); },
+
+			'populate': function (model, element) {
+				$(element)
+					.find('.name a').text(model.get('author').name).attr('href', model.get('author').url).end()
+					.find('img').attr('src', model.get('author').avatar).end()
+					.find('.content').html(model.get('text')).end()
+					.find('time').text(model.get('timestamp').toString()).end();
+			},
+
+			'itemEvents': {
+				'click button': function (model) {
+					if (!approvedTweets.get(model.get('id'))) {
+
+						$.ajax({
+							'url': nodeUrl + 'twitter-feed',
+							'type': 'POST',
+							'dataType': 'text',
+							'cache': false,
+							'data': JSON.stringify(model.toJSON()),
+							'headers': { 'Authorization': window.authToken }
+						});
+
+					}
+				}
+			}
+
+		});
+
+		 approvedTweetsView = new CollectionView({
+
+			'collection': approvedTweets,
+			'el': $('#twitter .twitter-feed ul').get(0),
+			'template': $('#twitter .twitter-feed ul > li.template').remove().removeClass('template'),
+
+			'advancedFilter': function (collection) { return collection.first(approvedTweetCount); },
+
+			'populate': function (model, element) {
+				$(element)
+					.find('.name a').text(model.get('author').name).attr('href', model.get('author').url).end()
+					.find('img').attr('src', model.get('author').avatar).end()
+					.find('.content').html(model.get('text')).end()
+					.find('time').text(model.get('timestamp').toString()).end();
+			},
+
+			'itemEvents': {
+				'click button': function (model) {
+
+					$.ajax({
+						'url': nodeUrl + 'twitter-feed/' + model.id,
+						'type': 'DELETE',
+						'dataType': 'text',
+						'cache': false,
+						'headers': { 'Authorization': window.authToken }
+					});
+
+				}
+			}
+
+		});
+
+		incomingTweetsView.render();
+		approvedTweetsView.render();
+
+		// Bind the AJAX call for changing the filter for streaming tweets
+		$('#twitter .filter button').on('click', function () {
+			$.ajax({
+				'url': nodeUrl + 'app-options',
+				'type': 'POST',
+				'dataType': 'text',
+				'cache': false,
+				'data': {'twitterSearch': $('#twitter .filter input').val().replace(/^\s+|\s+$/g, "")},
+				'headers': { 'Authorization': window.authToken }
+			});
+		});
+
+		/* DATA STREAM HANDLING
+		 ********************************** */
+
+		// General event stream
+		new easyXDM.Socket({
+
+			'interval': 1000,
+			'local': '../common/scripts/vendor/easyXDM/name.html',
+			'swf': '../common/scripts/vendor/easyXDM.swf',
+			'swfNoThrottle': true,
+			'remote': nodeUrl + 'stream?' + (new Date()).getTime(),
+			'onMessage': function (message) {
+
+				try {
+
+					var data = JSON.parse(message),
+						ev = data.event.split(':', 2),
+						el, temp;
+
+					// Product feed updater
+					if (ev[0] === 'productFeed') {
+
+						if (ev[1] === 'reset') {
+							productFeed.reset(data.payload);
+						} else if (ev[1] === 'add') {
+							if (!productFeed.get(data.payload.id)) { productFeed.add(data.payload); }
+						} else if (ev[1] === 'remove') {
+							var model = productFeed.get(data.payload.id);
+							if (model) { productFeed.remove(model); }
+						}
+
+					} else if (ev[0] === 'twitterFeed') {
+
+						if (ev[1] === 'reset') {
+							approvedTweets.reset(data.payload, {'parse': true, 'validate': true});
+						} else if (ev[1] === 'add') {
+							if (!approvedTweets.get(data.payload.id)) { approvedTweets.add(data.payload, {'parse': true, 'validate': true}); }
+						} else if (ev[1] === 'remove') {
+							var model = approvedTweets.get(data.payload.id);
+							if (model) { approvedTweets.remove(model); }
+						}
+
+					// Generic app options updater
+					} else if (ev[0] === 'appOptions') {
+
+						// Hangout links and stream URLS
+						if (_.has(data.payload, 'hangoutEmbed')) {
+							if (data.payload.hangoutEmbed && data.payload.hangoutEmbed.length) {
+								$('#general .hangouts .embed')
+									.attr('href', data.payload.hangoutEmbed)
+									.html(data.payload.hangoutEmbed);
+							} else {
+								$('#general .hangouts .embed')
+									.removeAttr('href')
+									.html('NOT AVAILABLE, START ON-AIR HANGOUT FIRST');
+							}
+						}
+						if (_.has(data.payload, 'mainHangoutLink')) {
+							if (data.payload.mainHangoutLink && data.payload.mainHangoutLink.length) {
+								temp = data.payload.mainHangoutLink.replace(/\gid=[0-9]+/, defaultMainLink.match(/\gid=[0-9]+/)[0]);
+								$('#general .hangouts .main')
+									.attr('href', temp)
+									.html(temp);
+							} else {
+								$('#general .hangouts .main')
+									.attr('href', defaultMainLink)
+									.html('NO HANGOUT - CLICK HERE TO START ONE');
+							}
+						}
+						if (_.has(data.payload, 'checkHangoutLink')) {
+							if (data.payload.checkHangoutLink && data.payload.checkHangoutLink.length) {
+								temp = data.payload.checkHangoutLink.replace(/\gid=[0-9]+/, defaultStagingLink.match(/\gid=[0-9]+/)[0]);
+								$('#general .hangouts .staging')
+									.attr('href', temp)
+									.html(temp);
+							} else {
+								$('#general .hangouts .staging')
+									.attr('href', defaultStagingLink)
+									.html('NO HANGOUT - CLICK HERE TO START ONE');
+							}
+						}
+
+						// Live message updates
+						el = $('#general .live-message textarea');
+						if (_.has(data.payload, 'liveMessage') && (data.payload.liveMessage !== el.val())) {
+							el.val(data.payload.liveMessage);
+						}
+
+					} else if (ev[0] === 'message') { window.alert(data.payload); }
+
+				} catch (e) {}
+
+			}
+
+		});
+
+		// Incoming tweets stream
+		new easyXDM.Socket({
+
+			'interval': 1000,
+			'local': '../common/scripts/vendor/easyXDM/name.html',
+			'swf': '../common/scripts/vendor/easyXDM.swf',
+			'swfNoThrottle': true,
+			'remote': nodeUrl + 'twitter-stream?' + (new Date()).getTime(),
+			'onMessage': function (message) {
+				try {
+					var data = JSON.parse(message);
+					if (data.event === 'tweet') { incomingTweets.add(data.payload, {'parse': true, 'validate': true}); }
+				} catch (e) {}
+			}
+
+		});
 
 	});
 
